f6a6addabc476c3a323eb279964a33f0a5ce00ed
sogbdn <45469439+sogbdn@users.noreply.github.com>
2019-01-10 16:19:16 -0500

    Add files via upload

---
+++ arrayMapping.js
@@ -0,0 +1,16 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+// var result = input.map(Math.sqrt(Math.pow(input[0].x, 2) + Math.pow(input[0].y, 2)));
+// var result = input.map(Math.sqrt(Math.pow(input[1].x, 2) + Math.pow(input[1].y, 2)));
+
+var result = input.map(function(factors) {
+  var value = Math.sqrt(Math.pow(factors.x, 2) + Math.pow(factors.y, 2));
+  return value;
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
